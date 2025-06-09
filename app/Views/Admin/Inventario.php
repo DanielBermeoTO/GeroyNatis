a4df2c6e6ddd0f5fe78f043a54cc7c054b412735
DanielBermeoTO <165852640+DanielBermeoTO@users.noreply.github.com>
2025-06-08 22:51:52 -0500

    Update Inventario.php

--- app/Views/Admin/Inventario.php
+++ app/Views/Admin/Inventario.php
@@ -3,7 +3,7 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 
-    require_once __DIR__ . '/../../Controllers/controladorInventario3.php';
+    require_once __DIR__ . '/../../Controllers/controladorInventario.php';
 } ?>
 <!doctype html>
 <html lang="en">
