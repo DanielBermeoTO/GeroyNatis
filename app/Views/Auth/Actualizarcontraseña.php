dffb1c1e754763a5b2aad5a9a852870062b0fa11
DanielBermeoTO <felipedanieltorres32@gmail.com>
2025-06-09 14:41:26 -0500

    holi

--- app/Views/Auth/Actualizarcontraseña.php
+++ app/Views/Auth/Actualizarcontraseña.php
@@ -5,11 +5,7 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 
 
 use App\Config\Database;
-use Dotenv\Dotenv;
 
-// Ajusta la ruta a la raíz de tu proyecto (normalmente 2 niveles arriba si estás en app/Models)
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../../../');
-$dotenv->load();
 
 $mysqli = Database::getConnection();
 
