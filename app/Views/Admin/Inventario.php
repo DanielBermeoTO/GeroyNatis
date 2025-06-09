4ed003ea7c9d5fcddc866b416f276ffc0c0f4fab
DanielBermeoTO <felipedanieltorres32@gmail.com>
2025-06-08 22:38:54 -0500

    d

--- app/Views/Admin/Inventario.php
+++ app/Views/Admin/Inventario.php
@@ -10,8 +10,8 @@ if (session_status() === PHP_SESSION_NONE) {
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../../public/css/Geroyn.css">
-      <link rel="stylesheet" href="../../public/css/pie.css">
+  <link rel="stylesheet" href="../../../public/css/Geroyn.css">
+      <link rel="stylesheet" href="../../../public/css/pie.css">
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
