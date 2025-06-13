804ccd6b169aa6785e4fd9f4dd76c8ad008d6217
DanielBermeoTO <felipedanieltorres32@gmail.com>
2025-06-13 14:51:52 -0500

    Duplications

--- app/Views/Web/UsuarioAñadirVenta.php
+++ app/Views/Web/UsuarioAñadirVenta.php
@@ -1,7 +1,8 @@
 <?php
 // Iniciar sesión
 session_start();
- require_once __DIR__ . '/../../Controllers/controladorInventario3.php'; ?>
+ require_once __DIR__ . '/../../Controllers/controladorInventario3.php'; 
+ ?>
 
 
 <!doctype html>
@@ -471,11 +472,6 @@ if (!isset($_SESSION['sesion']) || $_SESSION['sesion'] == "" || $_SESSION['rol']
       });
     });
   </script>
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
 </body>
 
 </html>
